2eb116e9a4785f0331818903b20591778b394c50
yukirin <yukirin_000@163.com>
2015-07-03 17:46:54 +0800

    123

--- NewProject/src/com/example/newproject/MainActivity.java
+++ NewProject/src/com/example/newproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.newproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("", "");
     }
 
 
