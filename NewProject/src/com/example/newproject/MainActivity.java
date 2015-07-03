58beab95abb7ade4d667216ae82c13d18e06f212
Nicky_Liu <liufh8989@foxmail.com>
2015-07-03 17:50:13 +0800

    test

--- NewProject/src/com/example/newproject/MainActivity.java
+++ NewProject/src/com/example/newproject/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("", "");
+        Log.i("", "test");
     }
 
 
